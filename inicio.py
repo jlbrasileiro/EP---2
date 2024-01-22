837effe38eef8c1ff309f2aa5b64558241be5b0d
Láisa Camilly <laisaco1@al.insper.edu.br>
2024-01-22 15:33:43 -0300

    Interação do jogo LÁISA

---
+++ inicio.py
@@ -0,0 +1,3 @@
+print(f'\n{"*"*10} Bem vindas(os) ao jogo \033[1;31;43m Batalha Naval \033[m !!! {"*"*10}\n')
+print('\n• Seu objetivo será tentar afundar todos os navios inimigos.\n• Posicione estrategicamente seus navios no começo do jogo e conte com a sorte! :)\n')
+print(f'\n\033[1;31;43m Navios disponivéis:\033[m\n➞ 4 Submarinos de tamanho 1.\n➞ 3 Contratorpedeiro de tamanho 2.\n➞ 2 Navio-Tanque de tamanho 3.\n➞ 1 Porta-aviões de tamanho 4.')
