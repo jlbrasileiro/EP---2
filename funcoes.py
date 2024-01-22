caa549f0c974eb4210dfd48e035cad466cdd9b27
jlbrasileiro <lucas_jl07@hotmail.com>
2024-01-22 16:16:22 -0300

    posiciona_frota e faz_jpgada João Lucas Brasileiro

--- funcoes.py
+++ funcoes.py
@@ -26,3 +26,33 @@ def preenche_frota(dados_de_posicionamento, nome_navio, frota):
     frota.append(navio)
 
     return frota 
+def faz_jogada(tabuleiro,linha,coluna):
+    if tabuleiro[linha][coluna] == 1:
+        tabuleiro[linha][coluna] = 'X'
+    if tabuleiro[linha][coluna] == 0:
+        tabuleiro[linha][coluna] = '-'
+    return tabuleiro
+
+def posiciona_frota(frota):
+    tabuleiro = [
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    ]
+
+    for navio in frota:
+        for coordenada in navio['posicoes']:
+            tabuleiro[coordenada[0]][coordenada[1]] = 1
+
+    return tabuleiro
+
+
+
+    
